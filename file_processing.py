7024fe836c6a6cfc39150e58b0a6658ac9616351
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2023-07-25 13:15:10 -0400

    Got the annotions into the df

--- file_processing.py
+++ file_processing.py
@@ -8,59 +8,71 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import imghdr
 
-#Create the Dictionary for the annotations 
-annotations = {
-"Filename":[], 
-"Points":[], 
-"LeftEye":[], 
-"RightEye":[], 
-"Mouth":[], 
-"LeftEar1":[], 
-"LeftEar2":[],
-"LeftEar3":[],
-"RightEar1":[], 
-"RightEar2":[], 
-"RightEar3":[] 
-}
+# #Create Empty Annotations Lists
+# filename_pts = [] 
+# points_pts = [] 
+# lefteye_pts = []
+# righteye_pts = [] 
+# mouth_pts = []
+# leftear1_pts = []
+# leftear2_pts = [] 
+# leftear3_pts = []
+# rightear1_pts = [] 
+# rightear2_pts = [] 
+# rightear3_pts = []
 
-#Create the dataframe 
-df = pd.DataFrame(annotations)
+# #Create the Dictionary for the annotations 
+# annotations = {
+# "Filename":filename_pts, 
+# "Points":points_pts, 
+# "LeftEye":lefteye_pts, 
+# "RightEye":righteye_pts, 
+# "Mouth":mouth_pts, 
+# "LeftEar1":leftear1_pts, 
+# "LeftEar2":leftear2_pts,
+# "LeftEar3":leftear3_pts,
+# "RightEar1":rightear1_pts, 
+# "RightEar2":rightear2_pts, 
+# "RightEar3":rightear3_pts 
+# }
 
-print(df)
+# df = pd.DataFrame(annotations)
 
 #Convert .cat file to a pandas df. 
-def read_cat_file(filename):
-    """Reads a .cat file and returns a Pandas DataFrame."""
-
-    df = pd.DataFrame()
-    with open(filename, 'r') as f:
-        for line in f:
-            row = line.split(' ')
-            df = df.append({
-                'Left Eye': row[1:3],
-                'Right Eye': row[3:5],
-                'Mouth': row[5:7],
-                'Left Ear-1': row[7:9],
-                'Left Ear-2': row[9:11],
-                'Left Ear-3': row[11:13],
-                'Right Ear-1': row[13:15],
-                'Right Ear-2': row[15:17],
-                'Right Ear-3': row[17:19]
-            }, ignore_index=True)
-    return df 
 
 #File Traversal
 def traverse_files(path):
+    df = pd.DataFrame()
+
     for root, directories, files in os.walk(path):
         for file in files:
-            # print(root,'\n',file,'\n-----------')
-            file_extension = os.path.splitext(file)[1]
+            file_name, file_extension = os.path.splitext(file)
             if file_extension == ".cat":
-                
+                #Read and sort cat data into df
+                with open(root+'/'+file, 'r') as f:
+                    for line in f:
+                        file_name = os.path.splitext(file)[0]
+                        row = line.split(' ')
+                        df = df.append({
+                            'Filename': file_name,
+                            'Points': row[0],
+                            'Left Eye': row[1:3],
+                            'Right Eye': row[3:5],
+                            'Mouth': row[5:7],
+                            'Left Ear-1': row[7:9],
+                            'Left Ear-2': row[9:11],
+                            'Left Ear-3': row[11:13],
+                            'Right Ear-1': row[13:15],
+                            'Right Ear-2': row[15:17],
+                            'Right Ear-3': row[17:19]
+                        }, ignore_index=True)
             else:
                 continue
-
+    return df
 
 #Only Runs if running this module. Will not run if imported elsewhere. 
 if __name__ == "__main__":
-	traverse_files('archive')
+	print(traverse_files('archive'))
+    
+
+    
