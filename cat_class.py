f381bfcd4f4c0260ef11e1ddd37b663eca371bf7
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2023-07-25 15:46:45 -0400

    Starting to build the Cat dataset Class

--- cat_class.py
+++ cat_class.py
@@ -8,6 +8,8 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import imghdr
 
+import file_processing.traverse_files
+
 
 # Ignore warnings
 import warnings
@@ -20,31 +22,9 @@ plt.ion()   # interactive mode
 #Read it, store the image name in img_name and store its annotations in 
 #an (L, 2) array landmarks where L is the number of landmarks in that row.
 
+cat_df = traverse_files('archive')
 
 ######Helper Functions########
-
-#Convert .cat file to a pandas df. 
-def read_cat_file(filename):
-    """Reads a .cat file and returns a Pandas DataFrame."""
-
-    df = pd.DataFrame()
-    with open(filename, 'r') as f:
-        for line in f:
-            row = line.split(' ')
-            df = df.append({
-                'Left Eye': row[1:3],
-                'Right Eye': row[3:5],
-                'Mouth': row[5:7],
-                'Left Ear-1': row[7:9],
-                'Left Ear-2': row[9:11],
-                'Left Ear-3': row[11:13],
-                'Right Ear-1': row[13:15],
-                'Right Ear-2': row[15:17],
-                'Right Ear-3': row[17:19]
-            }, ignore_index=True)
-    return df
-# annotations = read_cat_file('archive/CAT_00/00000001_000.jpg.cat')
-# print(annotations['Left Eye'][0][1])
 
 #Plots feature points on the images. 
 def show_landmarks(image, landmarks):
@@ -57,15 +37,8 @@ def show_landmarks(image, landmarks):
 #                landmarks)
 # plt.show()
 
-#File Traversal
-def traverse_files(path):
-    for root, directories, files in os.walk(path):
-        for file in files:
-            print(root,'\n',file,'\n-----------')
-
 #Only Runs if running this module. Will not run if imported elsewhere. 
 if __name__ == "__main__":
-	traverse_files('archive')
 
 
 #https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
