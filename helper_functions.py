2bbcbfe417fb97b7ff5cc5f9a2c40b1b23729d48
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2023-08-02 14:33:24 -0400

    Adjusted the pause time in show_landmarks

--- helper_functions.py
+++ helper_functions.py
@@ -9,7 +9,7 @@ def show_landmarks(image, landmarks):
     """Show image with landmarks"""
     plt.imshow(image)
     plt.scatter(landmarks[:, 0], landmarks[:, 1], s=10, marker='.', c='r')
-    plt.pause(5)  # pause a bit so that plots are updated
+    plt.pause(1)  # pause a bit so that plots are updated
 
 def get_catimages(root_dirs):
     datasets = []
